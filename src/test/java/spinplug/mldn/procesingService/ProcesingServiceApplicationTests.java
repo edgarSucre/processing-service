c21c301047ab52cd637089ef338688d348dc1986
edgarsucre <edgar.sucre.02@gmail.com>
2018-08-08 18:29:27 -0500

    first commit

---
+++ src/test/java/spinplug/mldn/procesingService/ProcesingServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package spinplug.mldn.procesingService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProcesingServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
