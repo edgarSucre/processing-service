48c1945081035e140a3997f28601e00e5d8cac31
edgarsucre <edgar.sucre.02@gmail.com>
2018-08-11 14:11:11 -0500

    add comment to implement hystrix

--- src/main/java/spinplug/mldn/procesingService/ProcesingServiceApplication.java
+++ src/main/java/spinplug/mldn/procesingService/ProcesingServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 public class ProcesingServiceApplication {
 
 	//TODO: when using feing, create user context interceptor for feing, to propagate the correlation id
+    //TODO: add hystrix
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcesingServiceApplication.class, args);
